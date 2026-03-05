a86b5a483511c216d7b15e6b7fd2da61172879b1
greensaltt <16310800+green-saltt@user.noreply.gitee.com>
2026-03-05 17:34:21 +0800

    项目初始化

---
+++ sky-common/src/main/java/com/sky/constant/StatusConstant.java
@@ -0,0 +1,15 @@
+package com.sky.constant;
+
+/**
+ * 状态常量，启用或者禁用
+ */
+public class StatusConstant {
+
+    //启用
+    // final：不可改变值 static：表示ENABLE为一个类级别的常量
+    // 任意对象可以xxx.ENABLE直接访问
+    public static final Integer ENABLE = 1;
+
+    //禁用
+    public static final Integer DISABLE = 0;
+}
